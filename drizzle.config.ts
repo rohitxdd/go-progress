a4394e7e79d338e22ad3b84e804c04ca70227e4b
rohitxd <rohitsharma998850@gmail.com>
2024-09-06 17:44:28 +0530

    setting up drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,10 @@
+import "@/data/envConfig";
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./data/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.POSTGRES_URL!,
+  },
+});
