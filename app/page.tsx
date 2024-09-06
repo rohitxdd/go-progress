65ed0fac1a43e73b5ec0d2e614a2cb779e663cc4
rohitxd <rohitsharma998850@gmail.com>
2024-09-06 14:31:01 +0530

    updating layout, fixed dark mode and removed text effect

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,12 @@
 import DailyProgress from "@/components/ui/DailyProgress";
 import { ModeToggle } from "@/components/ui/mode-toggle";
-import { TextEffect } from "@/components/ui/TextEffect";
-import { Progress } from "@radix-ui/react-progress";
-import Image from "next/image";
+import { Progress } from "@/components/ui/progress";
 
 export default function Home() {
   return (
     <div className="container md:px-4 px-2 py-1  mx-auto max-w-screen-lg">
       <nav className="flex h-12 justify-between items-center">
-        <p className="text-xl font-semibold antialiased">
-          <TextEffect per="char" preset="fade">
-            100 Days of Code
-          </TextEffect>
-        </p>
+        <p className="text-xl font-semibold antialiased">100 Days of Code</p>
         <ModeToggle />
       </nav>
       <div className="py-5 my-5 max-w-screen-lg mx-auto">
--- components/ui/DailyProgress.tsx
+++ components/ui/DailyProgress.tsx
@@ -6,29 +6,28 @@ import {
   CardTitle,
 } from "./card";
 
-const arr = new Array<Number>(100).fill(0);
+const arr = new Array<number>(100).fill(0);
 
 export default function DailyProgress() {
   return (
     <>
       <div className="mt-10 container m-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {arr.map((v, i) => (
-          <Card className="cursor-pointer border-2 shadow-sm hover:shadow-2xl transition-all ease-in-out duration-200 dark:hover:scale-105" key={i}>
+          <Card
+            className="cursor-pointer border-2 shadow-sm hover:shadow-2xl transition-all ease-in-out duration-200 dark:hover:scale-105"
+            key={i}
+          >
             <CardHeader>
-              <CardTitle>Day 1</CardTitle>
-              <CardDescription></CardDescription>
+              <CardTitle className="text-xl font-se">Day 1</CardTitle>
+              <CardDescription>Go Basic</CardDescription>
             </CardHeader>
-            <CardContent className="grid gap-4">
-              <div className=" flex items-center space-x-4 rounded-md border p-4">
-                <div className="flex-1 space-y-1">
-                  <p className="text-sm font-medium leading-none">
-                    Push Notifications
-                  </p>
-                  <p className="text-sm text-muted-foreground">
-                    Send notifications to device.
-                  </p>
-                </div>
-              </div>
+            <CardContent className="grid">
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id
+                architecto dolores necessitatibus ad, animi enim iste nulla odio
+                velit adipisci similique dolore natus, harum aliquam eos
+                accusantium neque
+              </p>
             </CardContent>
           </Card>
         ))}
