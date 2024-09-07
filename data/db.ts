4680014d9d07a69f6dbe5d6f4243318955fc8990
Rohit Sharma <rohitsharma998850@gmail.com>
2024-09-07 16:07:04 +0530

    fix: build error because of allowImportingTsExtensions

--- data/db.ts
+++ data/db.ts
@@ -1,4 +1,4 @@
-import "../envConfig.ts";
+import "../envConfig";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 const client = postgres(process.env.POSTGRES_URL!, { prepare: false });
