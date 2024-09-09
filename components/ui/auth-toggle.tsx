2f04ca5155e353befd36d74a493daf907b4bcf79
rohitxd <rohitsharma998850@gmail.com>
2024-09-09 16:59:17 +0530

    adding add-new page link in nav bar

--- components/ui/auth-toggle.tsx
+++ components/ui/auth-toggle.tsx
@@ -1,13 +1,28 @@
 import { auth } from "@/auth";
 import SignIn from "../sign-in";
 import { SignOut } from "../sign-out";
+import { MdEdit } from "react-icons/md";
+import { Button } from "./button";
+import Link from "next/link";
 
 export default async function AuthSwitch() {
   const session = await auth();
-  console.log(session);
   if (!session?.user) {
     return <SignIn />;
   } else {
+    if (session?.user?.email === process.env.ADMIN_MAIL) {
+      return (
+        <>
+          <Link href="/add-new">
+            <Button variant="outline">
+              <MdEdit />
+            </Button>
+          </Link>
+          <img src={session.user.image!} className="w-8 rounded-full" alt="X" />
+          <SignOut />
+        </>
+      );
+    }
     return (
       <>
         <img src={session.user.image!} className="w-8 rounded-full" alt="X" />
